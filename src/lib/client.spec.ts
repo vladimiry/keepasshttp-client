ef3831ecc63f63cb4cca9091612dc6a537108cd2
Vladimir Y <dev.vladimir.y@gmail.com>
2017-08-30 07:49:01 +0300

    test password entry values decrypting

--- src/lib/client.spec.ts
+++ src/lib/client.spec.ts
@@ -1,23 +1,26 @@
 import * as test from "tape";
+import * as mockRequire from "mock-require";
+import {createCipheriv, randomBytes} from "crypto";
 
 import {KeePassHttpClient} from "./client";
+import {Response} from "./model";
 import {ErrorCode, TypedError} from "./model/common";
 
-const arg = {
+const clientOpts = {
     uri: "http://localhost:12345",
     keyId: {
         id: "id-",
-        // 32 bytes size, base64 encoded
-        key: "6ipg8p/immwgmzNN04pIZGlfE6QkoGykokilfc3vE3I=",
+        key: randomBytes(32).toString("base64"),
     },
 };
 
 test("constructor args should be setup as a member fields", (t) => {
-    const client = new KeePassHttpClient(arg);
+    const client = new KeePassHttpClient(clientOpts);
 
-    t.equal(client.uri, arg.uri);
-    t.equal(client.id, arg.keyId.id);
-    t.equal(client.key, arg.keyId.key);
+    t.equal(client.uri, clientOpts.uri);
+    t.equal(client.id, clientOpts.keyId.id);
+    t.equal(client.key, clientOpts.keyId.key);
+
     t.end();
 });
 
@@ -50,7 +53,7 @@ test("'getLogins/getLoginsCount/setLogin' methods should fail if 'id' has not be
 test("requests methods should return a promises", (t) => {
     const isPromise = (object: any) => object && typeof object.then === "function"
         && typeof object.catch === "function";
-    const client = new KeePassHttpClient(arg);
+    const client = new KeePassHttpClient(clientOpts);
 
     t.ok(isPromise(client.testAssociate()));
     t.ok(isPromise(client.associate()));
@@ -59,4 +62,84 @@ test("requests methods should return a promises", (t) => {
     t.ok(isPromise(client.setLogin({url: "", uuid: "", login: "", password: ""})));
 
     t.end();
+});
+
+test("values of the password entry should be properly decrypted", (t) => {
+    t.plan(6);
+
+    const expectedEntriesResponse = {
+        Success: true,
+        Nonce: "",
+        Verifier: "",
+        Entries: [
+            {
+                Name: "name",
+                Login: "login",
+                Password: "password",
+                Uuid: "uuid",
+            } as Response.Entry,
+        ],
+    };
+    const encrypt = (key: string, iv: string, data: string) => {
+        const cipher = createCipheriv(
+            "aes-256-cbc",
+            Buffer.from(key, "base64"),
+            Buffer.from(iv, "base64"),
+        );
+
+        return Buffer
+            .concat([cipher.update(Buffer.from(data, "utf8")), cipher.final()])
+            .toString("base64");
+    };
+    const clientModule = "./client";
+    const mockedModule = "request-promise-native";
+
+    mockRequire(mockedModule, {
+        post: () => {
+            return new Promise((resolve) => {
+                const key = clientOpts.keyId.key;
+                const iv = randomBytes(16).toString("base64");
+
+                const httpResponse = JSON.parse(JSON.stringify(expectedEntriesResponse));
+                const httpResponseEntry = httpResponse.Entries[0];
+
+                httpResponse.Nonce = iv;
+                httpResponse.Verifier = encrypt(key, iv, iv);
+
+                httpResponseEntry.Name = encrypt(key, iv, httpResponse.Entries[0].Name as string);
+                httpResponseEntry.Login = encrypt(key, iv, httpResponse.Entries[0].Login as string);
+                httpResponseEntry.Password = encrypt(key, iv, httpResponse.Entries[0].Password as string);
+                httpResponseEntry.Uuid = encrypt(key, iv, httpResponse.Entries[0].Uuid);
+
+                resolve(httpResponse);
+            });
+        },
+    });
+
+    // uncache previously loaded library
+    delete require.cache[require.resolve(clientModule)];
+
+    const reImportedKeePassHttpClient = require(clientModule).KeePassHttpClient;
+    const client = new reImportedKeePassHttpClient(clientOpts);
+
+    client.getLogins({url: ""})
+        .then((response: Response.Complete) => {
+            if (!response || !response.Entries) {
+                t.fail("Invalid response");
+                return;
+            }
+
+            const entry = response.Entries[0];
+            const expectedEntry = expectedEntriesResponse.Entries[0];
+
+            t.ok(response);
+            t.ok(response.Entries && response.Entries.length === 1);
+
+            t.equal(entry.Uuid, expectedEntry.Uuid);
+            t.equal(entry.Name, expectedEntry.Name);
+            t.equal(entry.Login, expectedEntry.Login);
+            t.equal(entry.Password, expectedEntry.Password);
+
+            mockRequire.stop(mockedModule);
+        });
 });
--- src/lib/model/common.ts
+++ src/lib/model/common.ts
@@ -14,13 +14,13 @@ export enum ErrorCode {
 export class TypedError extends Error {
     constructor(message: string, public code?: ErrorCode) {
         super(message);
-        Object.setPrototypeOf(this, ErrorResponse.prototype);
+        Object.setPrototypeOf(this, new.target.prototype);
     }
 }
 
 export class ErrorResponse<T extends Response.Base> extends TypedError {
     constructor(message: string, public request: Request.Request, public response: T, code?: ErrorCode) {
         super(message, code);
-        Object.setPrototypeOf(this, ErrorResponse.prototype);
+        Object.setPrototypeOf(this, new.target.prototype);
     }
 }
